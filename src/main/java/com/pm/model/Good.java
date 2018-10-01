f1b394857c5e7553e58d10bafcbbabaed4f10121
linyuchao <1765301546@qq.com>
2018-10-01 17:38:21 +0800

    linyuchao17:38

--- src/main/java/com/pm/model/Good.java
+++ src/main/java/com/pm/model/Good.java
@@ -9,7 +9,6 @@ public class Good {
 	 * good_type			物品类型
 	 * good_price			单价
 	 * good_count			数量
-	 * good_sum_price		金额
 	 * good_repetory		计算库存
 	 * good_register_date	登记时间
 	 * good_notes			备注
@@ -19,7 +18,6 @@ public class Good {
 	private String good_type;
 	private double good_price;
 	private int good_count;
-	private double good_sum_price;
 	private int good_repetory;
 	private Date good_register_date;
 	private String good_notes;
@@ -53,12 +51,6 @@ public class Good {
 	public void setGood_count(int good_count) {
 		this.good_count = good_count;
 	}
-	public double getGood_sum_price() {
-		return good_sum_price;
-	}
-	public void setGood_sum_price(double good_sum_price) {
-		this.good_sum_price = good_sum_price;
-	}
 	public int getGood_repetory() {
 		return good_repetory;
 	}
@@ -77,11 +69,6 @@ public class Good {
 	public void setGood_notes(String good_notes) {
 		this.good_notes = good_notes;
 	}
-	@Override
-	public String toString() {
-		return "Good [good_id=" + good_id + ", good_name=" + good_name + ", good_type=" + good_type + ", good_price="
-				+ good_price + ", good_count=" + good_count + ", good_sum_price=" + good_sum_price + ", good_repetory="
-				+ good_repetory + ", good_register_date=" + good_register_date + ", good_notes=" + good_notes + "]";
-	}
+	
 	
 }
