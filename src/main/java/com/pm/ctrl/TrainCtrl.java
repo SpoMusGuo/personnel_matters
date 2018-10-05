5e85fe74c6ab6c174106d6da95375dc3864d5b0f
fade007 <1070136914@qq.com>
2018-10-05 10:46:01 +0800

    10/5 10:45

--- src/main/java/com/pm/ctrl/TrainCtrl.java
+++ src/main/java/com/pm/ctrl/TrainCtrl.java
@@ -3,23 +3,27 @@ package com.pm.ctrl;
 import java.util.List;
 import java.util.Map;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import javax.annotation.Resource;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.pm.model.Train;
+import com.pm.model.Emp;
+import com.pm.model.Pager;
 import com.pm.model.Paging;
+import com.pm.model.Train;
+import com.pm.service.EmpService;
 import com.pm.service.TrainService;
 
-@Controller
+@Controller(value = "trainCtrl")
 public class TrainCtrl {
 	
 	@Autowired
-	private TrainService trainservice;
-
+	private EmpService empservice;
 	@RequestMapping("/index_train")
-	public String queryTrainList(String pageSize, String currentPage, List<Train> trainsa, Map map) {
+	public String queryEmpList(String pageSize, String currentPage, Map map) {
 
 		Integer iPageSize;
 		Integer iCurrentPage;
@@ -34,45 +38,11 @@ public class TrainCtrl {
 		} else {
 			iCurrentPage = 1;
 		}
-		List<Train> trains = trainservice.listAll();
+		List<Emp> emps = empservice.queryEmpList();
 		
-		map.put("TRAINS", trains);
-		map.put("PAGING", new Paging(trains.size(), iCurrentPage, iPageSize));
-		return "index_train";
+		map.put("EMPS", emps);
+		map.put("PAGING", new Paging(emps.size(), iCurrentPage, iPageSize));
+		return "train/index_train";
 	}
 
 }
-
-
-/*@Controller
-public class TrainCtrl {
-	@Autowired
-	private TrainService trainService;
-
-	@RequestMapping("/index_train")
-	public String list(Model model, Integer currentPageindex) {
-		Pager<Train> pager = new Pager<Train>();
-		// 从前端传来的当前页和每页记录条数
-		int pagesize = 2;
-		if (currentPageindex == null) {
-			currentPageindex = 1;
-		}
-		int startindex = (currentPageindex - 1) * pagesize;
-		int records = trainService.getRecords();
-		int pagecount = (int) Math.ceil(records / pagesize);
-		pager.setPagesize(pagesize);
-		pager.setPageindex(currentPageindex);
-		pager.setStartindex(startindex);
-		pager.setPagecount(pagecount);
-		pager.setRecords(records);
-		pager.setDatas(trainService.listPager(pager));
-		model.addAttribute("pager", pager);
-		return "index_train";
-	}
-
-	@RequestMapping("/addTrain")
-	public String addTrain(Model model) {
-		return "index_train";
-	}
-
-}*/
